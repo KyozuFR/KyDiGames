ea888657351478a8a753122876c13c56bc50fc2d
JLinares <julien@2linares.fr>
2026-01-06 22:04:03 +0100

    First Commit

---
+++ src/main/java/fr/kydi/kydigames/Kydigames.java
@@ -0,0 +1,10 @@
+package fr.kydi.kydigames;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Kydigames implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
